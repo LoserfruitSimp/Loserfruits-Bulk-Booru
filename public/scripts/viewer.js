17bd41e822ac21f5a9cf6733c6af52ca5254d22c
Glitch (spiider34) <none>
2023-09-23 00:22:36 +0000

    🍁🌍 Checkpoint ./public/scripts/viewer.js:49008067/20312

--- public/scripts/viewer.js
+++ public/scripts/viewer.js
@@ -16,45 +16,53 @@ function convertURL(url) {
 }
 
 var tagData = [];
-getData(tagsQ, 0);
 
-async function getData(tags, PID) {
-  for (let urlI = 0; urlI < urls.length; urlI++) {
-    const response = await fetch(
-      `https://${hostURL}/posts?tags=${tags}&sourse=${urls[urlI]}&pid=${PID}`
-    );
-
-    const data = await response.json();
-
-    if (Array.isArray(data)) {
-      tagData = data;
-    } else {
-      tagData = data.post;
-    }
-
-    console.log(tagData);
-
-    for (var i = 0; i < tagData.length; i++) {
-      const figure = document.createElement("figure");
-      const img = document.createElement("img");
-
-      if (
-        tagData[i].file_url.endsWith(".webm") ||
-        tagData[i].file_url.endsWith(".mp4")
-      ) {
-        img.style = "border: solid red; border-width: thin;";
+async function webResponces() {
+  for (let i = 0; i < urls.length; i++) {
+    async function getAll(index) {
+      const data = await getData(urls[i], tagsQ, index);
+      tagData = tagData.concat(data);
+      if (data.length === 100) {
+        await getAll(index + 100);
       }
-
-      img.src = convertURL(tagData[i].preview_url);
-      img.classList.add("galleryItem");
-      img.onclick = function () {
-        click(img);
-      };
-      img.id = i;
-
-      figure.classList.add("hover");
-      figure.appendChild(img);
-      gallery.appendChild(figure);
     }
+    await getAll(0);
+  }
+
+  for (var i = 0; i < tagData.length; i++) {
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+
+    if (
+      tagData[i].file_url.endsWith(".webm") ||
+      tagData[i].file_url.endsWith(".mp4")
+    ) {
+      img.style = "border: solid red; border-width: thin;";
+    }
+
+    img.src = convertURL(tagData[i].preview_url);
+    img.classList.add("galleryItem");
+    img.onclick = function () {
+      click(img);
+    };
+    img.id = i;
+
+    figure.classList.add("hover");
+    figure.appendChild(img);
+    gallery.appendChild(figure);
   }
 }
+
+async function getData(url, tags, PID) {
+  const response = await fetch(
+    `https://${hostURL}/posts?tags=${tags}&sourse=${url}&pid=${PID}`
+  );
+
+  const data = await response.json();
+  if (Array.isArray(data)) {
+    return data;
+  } else {
+    return data.post;
+  }
+}
+webResponces()
